882548167d40636b8c2beb9ccecd2cd497ce624a
乌迪 <wudi.ye@tuya.com>
2021-12-27 16:15:03 +0800

    jest config babel

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App: React.FC = () => {
+
+  return (
+    <View>
+      <Text>666</Text>
+    </View>
+  )
+}
+
+export default App;
--- src/dp.test.js
+++ src/dp.test.js
@@ -1,3 +1,5 @@
+import App from './App';
+
 const add = (a, b) => a + b;
 
 describe('test jest', () => {
